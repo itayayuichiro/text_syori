1d17ca419acfbc7c2a07f4317cae5f9c504eacbb
itaya_A <itaya_a@gmail.com>
2018-01-30 10:05:37 +0900

    とりあえず動くぞー

--- twpro_last.rb
+++ twpro_last.rb
@@ -7,30 +7,89 @@ class Twpro
   @@variables = {}
   @@functions = {}
   @@result = []
-
+  @@indent = 0
   def initialize
-    tokenize(ARGV[0])
+    f = open(ARGV[0])
+    @scanner = StringScanner.new(f.read)
+    parser
     eval
   end
   @@imps = {
     'ログイン' => :start,
     'ログアウト' => :end,
     'ツイート' => :print,
-    '3RT' => :loop,
+    'RT' => :loop,
     'リスト作成' => :make_function,
     'リスト' => :call_function,
     'フォロー' => :make_variable,
-
+    '  ' => :indent,
   }
+  def unget_token
+    @scanner.unscan
+  end
+  def parser
+    while !@scanner.eos?
+      @tokenize = tokenize
+      token = @@imps[@tokenize]
+      param1,param2 = nil
+      p "token:#{token}"
+      case token
+        # コードの始まり
+        when :start
+          block = [token]
+        # 出力
+        when :print
+          block = [token, tokenize]
+        # 関数生成
+        when :make_function
+          block = [token, tokenize]
+        # 関数
+        when :call_function
+          block = [token, tokenize]
+        # インデント
+        when :indent
+          @@indent = @@indent + 1
+          p "next#{@@indent}"
+          next
+        else
+            p "aaaa"
+            @tokenize2 = tokenize
+            token2 = @@imps[@tokenize2]
+            # 変数生成
+            if token2 == :make_variable 
+              block = [token2,@tokenize,tokenize]
+              token = :make_variable
+            # 繰り返し
+            elsif token2 == :loop
+              block = [token2,@tokenize.to_i]
+              token = :loop
+            end
+        end
+        if token == :start or token == :print or token == :loop or token == :make_function or token == :call_function or token == :make_variable 
+          if @@indent == 1
+            @@result[-1].push(block)
+            @@indent = 0
+          elsif @@indent == 2
+            @@result[-1][-1].push(block)
+            @@indent = 0
+          else
+            @@result.push(block)
+          end
+        end
+    end
+    p @@result
+  end
 
-  def tokenize(file)
-    f = open(file)
-  	#pattern = ARGV[0]
-  	f.each{|line|
-
-#      puts line
-
-      unless imp = line.scan(/ログイン|\d+RT|ツイート|リスト作成|リスト|ログアウト|フォロー/)[0]
+  def tokenize
+    token = @scanner.scan(/(ログイン|ツイート|リスト作成|リスト|ログアウト|フォロー|\'.+?\'|  |RT|:.+?\n|\n.+? |\d+|.+? | .+?\n|)[\n]*/)
+    unless token.match(/  /)
+      token = token.delete(" ")
+    end
+    p token.chomp
+      
+    return token.chomp
+=begin
+      unless imp = line.scan(/ログイン|ツイート|リスト作成|リスト|ログアウト|フォロー/)[0]
         raise Exception, '字句解析エラー'
       end
       # debug(imp)
@@ -70,12 +129,13 @@ class Twpro
       end
       }
       debug(@@result)
+=end
 	end
 
-  def parser(imp,param,param2)
-    @@result.push([imp,param,param2])
-    return [imp,param,param2]
-  end
+  # def parser(imp,param,param2)
+  #   @@result.push([imp,param,param2])
+  #   return [imp,param,param2]
+  # end
   def eval
     @@result.each do |block|
       send("evaluate_#{block[0].to_s.delete(':')}",block[1])
